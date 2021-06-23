e4e7133334f62a796d66dd77b7a2a488e70de037
miki takezawa <mikiaaamikimiki@gmail.com>
2021-06-23 14:15:58 +0900

    controller implementation completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,53 @@
 class PostsController < ApplicationController
+  before_action :set_post,only: [:edit,:update,:destroy]
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to new_post_path,notice: 'つぶやきを投稿しました!'
+      else
+        render :new
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    @post.update(post_params)
+    redirect_to posts_path,notice: 'つぶやきを編集しました!'
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path,notice: 'つぶやきを削除しました!'
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
