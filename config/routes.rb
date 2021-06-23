2f02376444ce5556d049b20ff4e5a49c359f558b
miki takezawa <mikiaaamikimiki@gmail.com>
2021-06-23 14:08:18 +0900

    create router

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
 end
